2de26e83e5417decbc753e1fc2da2903dbb0b532
khilil <khililpatel0704@gmail.com>
2025-01-18 10:29:35 +0530

    Initial commit

---
+++ Node JS/math.js
@@ -0,0 +1,22 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const div = (a, b) => {
+  return a / b;
+};
+
+const mult = (a, b) => {
+  return a * b;
+};
+
+const Pi = 3.214;
+
+// module.exports.add = add
+// module.exports.mult = mult
+
+module.exports = { add, mult, sub, div, Pi };
